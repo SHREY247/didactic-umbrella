e3853849e69f4673520266764b39f50354b09f5e
SHREY247 <108192833+SHREY247@users.noreply.github.com>
2023-01-03 14:10:14 +0530

    Create Buysellstocks(Leetcode121).cpp

---
+++ Buysellstocks(Leetcode121).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxRight = prices[n-1];
+        int maxProfit = 0;
+        for(int i=n-2;i>=0;i--){
+            maxRight=max(maxRight,prices[i]);
+            maxProfit=max(maxProfit,maxRight-prices[i]);
+
+        }
+        
+        return maxProfit;
+    }
+};

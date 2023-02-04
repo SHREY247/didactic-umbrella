328e77233572dcdf9187d1600fdfabdd52d7017c
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-02-05 00:34:22 +0530

    Update PermutationinString(Leetcode567).cpp

--- Strings/PermutationinString(Leetcode567).cpp
+++ Strings/PermutationinString(Leetcode567).cpp
@@ -1,6 +1,7 @@
 /*Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 In other words, return true if one of s1's permutations is the substring of s2.*/
 
+//Using sliding window technique
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {

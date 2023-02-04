8f7157ec34d21c7a67623f68853dc42aba01a5b7
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-02-05 00:32:48 +0530

    Update PermutationinString(Leetcode567).cpp

--- Strings/PermutationinString(Leetcode567).cpp
+++ Strings/PermutationinString(Leetcode567).cpp
@@ -3,19 +3,39 @@ In other words, return true if one of s1's permutations is the substring of s2.*
 
 class Solution {
 public:
-    
     bool checkInclusion(string s1, string s2) {
-        int m[26] = {0}; //character array
-        for(char c : s1) m[c - 'a']++;
-        int j = 0, i = 0, total_chars = s1.size();
-        while(j < s2.size()){
-            if(m[s2.at(j++) - 'a']-- > 0)
-                total_chars--;
-            if(total_chars == 0) return true;
-            //shifting of window
-            if(j - i == s1.size() && m[s2.at(i++) - 'a']++ >= 0)
-                total_chars++;
-        }
-        return false;
+      if(s1.size()>s2.size())
+      {
+          return false;
+      }
+      vector<int>v1(26,0);
+      vector<int>v2(26,0);
+      for(auto ch: s1)
+      {
+          v1[ch-'a']++;
+      }
+      int i = 0 , j = 0;
+      while(j<s2.size())
+      {
+         v2[s2[j]-'a']++;
+         if(j-i + 1 == s1.size())
+         {
+             if(v1 == v2)
+             {
+                 return true;
+             }
+         }
+         if(j-i+1 < s1.size())
+         {
+             j++;
+         }
+         else
+         {
+             v2[s2[i]-'a']--;
+             i++;
+             j++;
+         }
+      }
+      return false;
     }
 };

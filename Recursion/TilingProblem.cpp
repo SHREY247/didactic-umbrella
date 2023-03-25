75f9869e856bb56221efb62e123893de00c476ca
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-03-26 01:51:37 +0530

    Update TilingProblem.cpp

--- Recursion/TilingProblem.cpp
+++ Recursion/TilingProblem.cpp
@@ -8,7 +8,12 @@ using namespace std;
 int tiles(int n,int m){
     //If n is smaller than m, we have just 1 arrangement
     if(n<m) return 1;
-  
+  	
+	/*Example n=4, m=3
+	Suppose, we put first tile vertically, now, we are left with another subproblem tile(n-1,m)
+	If we put tiles horizontally then subproblem left is tiles(n-m, m)
+	Complete problem would be sum of both subproblems
+	*/
 	int op1 = tiles(n-1, m);
 	int op2 = tiles(n-m, m);
 	return (op1 + op2);

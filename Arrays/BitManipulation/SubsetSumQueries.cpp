3159cb967ce4bc44bbcf7429d35f3fe25f6e4e8e
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-05-01 21:58:45 +0530

    Update SubsetSumQueries.cpp

--- Arrays/BitManipulation/SubsetSumQueries.cpp
+++ Arrays/BitManipulation/SubsetSumQueries.cpp
@@ -1,38 +1,86 @@
-/*The Planet Earth is under a threat from the aliens of the outer space and your task is to defeat an enemy
-who is N steps above you (assume yourself to be at ground level i.e. at the 0th level). 
-You can take jumps in power of 2. In order to defeat the enemy as quickly as possible you have to reach the Nth step in minimum moves possible.*/
+/*Given an array arr[] and a number of queries, where in each query we have to check whether
+a subset whose sum is equal to given number exists in the array or not. 
 
-#include<bits/stdc++.h>
+Example:
+Input : arr[]   = {1, 2, 3};
+        query[] = {5, 3, 8}  
+Output : Yes, Yes, No
+There is a subset with sum 5, subset is {2, 3}
+There is a subset with sum 3, subset is {1, 2}
+There is no subset with sum 8.
+
+
+Approach:
+The idea is to use bitset container in C++. Using bitset, we can precalculate the existence 
+all the subset sums in an array in O(n) and answer subsequent queries in just O(1). We basically 
+use an array of bits bit[] to represent the subset sum of elements in the array. Size of bit[] 
+should be at least sum of all array elements plus 1 to answer all queries. */
+
+// C++ program to answer subset sum queries using bitset
+#include <bits/stdc++.h>
 using namespace std;
 
-long long convertDecimalToBinary(unsigned long long int n)
-{
-    long long binaryNumber = 0;
-    unsigned long long int remainder, i = 1, step = 1;
+// Maximum allowed query value
+# define MAXSUM 10000
 
-    while (n!=0)
-    {
-        remainder = n%2;
-        n /= 2;
-        binaryNumber += remainder*i;
-        i *= 10;
-    }
-    return binaryNumber;
+// function to check whether a subset sum equal to n
+// exists in the array or not.
+void processQueries(int query[], int nq, bitset<MAXSUM> bit)
+{
+// One by one process subset sum queries
+for (int i=0; i<nq; i++)
+{
+int x = query[i];
+
+// If x is beyond size of bit[]
+if (x >= MAXSUM)
+{
+cout << "NA, ";
+continue;
 }
 
-int earthLevel(int k)
-{
-    //your code goes here
-    unsigned long long int binaryNumber, sum = 0;
-    binaryNumber = convertDecimalToBinary(k);
-    
-    while (binaryNumber != 0)
-    {
-        unsigned long long int t;
-        t = binaryNumber%2;
-        sum = sum + t;
-        binaryNumber = binaryNumber/10;
-    }
-    
-    return sum;
+// Else if x is a subset sum, then x'th bit
+// must be set
+bit[x]? cout << "Yes, " : cout << "No, ";
 }
+}
+
+// function to store all the subset sums in bit vector
+void preprocess(bitset<MAXSUM> &bit, int arr[], int n)
+{
+// set all the bits to 0
+bit.reset();
+
+// set the 0th bit because subset sum of 0 exists
+bit[0] = 1;
+
+// Process all array elements one by one
+for (int i = 0; i < n; ++i)
+
+// Do OR of following two
+// 1) All previous sums. We keep previous value
+// of bit.
+// 2) arr[i] added to every previous sum. We
+// move all previous indexes arr[i] ahead.
+bit |= (bit << arr[i]);
+}
+
+// Driver program
+int main()
+{
+int arr[] = {3, 1, 5};
+int query[] = {8, 7};
+
+int n = sizeof(arr) / sizeof(arr[0]);
+int nq = sizeof(query) / sizeof(query[0]);
+
+// a vector of MAXSUM number of bits
+bitset<MAXSUM> bit;
+
+preprocess(bit, arr, n);
+processQueries(query, nq, bit);
+
+return 0;
+}
+
+

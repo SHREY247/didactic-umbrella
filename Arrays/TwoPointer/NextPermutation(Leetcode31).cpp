b22b53bfcc76baf81f45778810c050df88852561
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-06-04 01:30:29 +0530

    Create NextPermutation(Leetcode31).cpp

---
+++ Arrays/TwoPointer/NextPermutation(Leetcode31).cpp
@@ -0,0 +1,43 @@
+/*A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.*/
+
+/*Approach:
+1: Traverse from the end of array to find index, such that nums[i]<nums[i+1], and assign value of i to index
+2: Traverse from the end of array again, to find another index i such that, nums[i]>nums[index]
+3: swap(a[index], a[i])
+4: Reverse the elements from ind+1 to end
+
+*/
+
+/*Time Complexity: O(N)
+Space Complexity: O(1)*/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        
+        for(int i=n-1; i>=index && index != -1; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};

05d61ab3376feb2190faa1a986203a1743c589fc
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-07-07 18:03:36 +0530

    Update Permutations(Leetcode46).cpp

--- Backtracking/Permutations(Leetcode46).cpp
+++ Backtracking/Permutations(Leetcode46).cpp
@@ -3,19 +3,25 @@
 class Solution {   
 private :
     void permute(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, int index){
+        //Base case: When index becomes equal to size of nums push_back temp into ans
         if(index == nums.size()){
             ans.push_back(temp);
             return;
         }
 
         //Swap for different permutations
+        //Example For 1st iteration
+        //swap 0th element with 0th element and then print all permutations from index+1 
+        //Then unswap so that original array remains unchanged
+        //Now, swap 0th element with 1st element and print permutations and similarly the loop goes on uptil i=temp.size() - 1
         for(int i=index; i<temp.size(); i++){
-            swap(temp[index] , temp[i]);
-            permute(nums, ans, temp, index+1);
+             swap(temp[index] , temp[i]);
+             permute(nums, ans, temp, index+1);
         //Swap back so that original array remains unchanged  
-            swap(temp[index] , temp[i]);
+             swap(temp[index] , temp[i]);
         }
     }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;

57d17e49671a648c241b82d6f7ec46705eac89cb
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-07-15 01:29:36 +0530

    Update SearchRotatedArray(Leetcode33).cpp

--- Arrays/Binary_Search/SearchRotatedArray(Leetcode33).cpp
+++ Arrays/Binary_Search/SearchRotatedArray(Leetcode33).cpp
@@ -6,41 +6,82 @@ Given the array nums after the possible rotation and an integer target, return t
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+
+    
+//Function to find the maximum point of the array
+int mainPoint(vector<int> arr,int n)
+{     
+	int l=0,h=n-1;
+		while(l<=h)
+		{
+			int mid=(l+h)/2;
+			//If last element is greater than middle one means maximum element would be lying in left part
+			if(arr[mid]<=arr[n-1])
+			{
+				h=mid-1;
+			}
+			else
+			{   
+				//If you find a point ehre the next element is smaller than the current one, this is the maximum
+				if(arr[mid]>arr[mid+1])
+				{
+					return mid;
+				}
+				
+				//Otherwise maximum would lie in the right part
+				else
+					l=mid+1;
+			}
+		}
+	//Return -1 for zero rotations case
+	return -1;
+}
+ 
+int findTarget(vector<int> arr,int p,int n,int target)
+{
+	int l,h;
+ 		//If target is lesser than the last element, target lies to the right of main Point
+		if(target<=arr[n-1])
+		{
+			l=p+1,h=n-1;
+		}
+	
+	    //Otherwise, target lies in between indexes 0 and p (inclusive)
+		else{
+			l=0,h=p;
+		}
+	
+	//Binary search
+	while(l<=h)
+	{
+		int mid= (l+h)/2;
+		if(arr[mid]==target)
+		{
+			return mid;
+		}
+		else if(arr[mid]>target)
+		{
+			h=mid-1;
+		}
+		else
+		{
+			l=mid+1;
+		}
+	}
+	return -1;
+}
+ 
+        int search(vector<int>& nums, int target) {
         int n=nums.size();
-
-        int s=0;
-        int e = n-1;
-
-        while(s<=e){
-            int mid=(s+e)/2;
-
-            if(nums[mid]==target){
-                return mid;
-            }
-
-            //2 cases, mid lies in part 1 or 2
-
-            //mid lies in the left side 
-            if(nums[s]<=nums[mid]){
-                if(target>=nums[s] && target<=nums[mid]){
-                    e=mid-1;
-                }
-                else{
-                    s=mid+1;
-                }
-
-            }
-            //mid lies in the right side
-            else{
-                if(target>=nums[mid] && target<=nums[e]){
-                        s=mid+1;
-                }
-                else{
-                    e=mid-1;
-                }
-            }
-        }
-        return -1;       
+        int p = mainPoint(nums,n);
+	    int index = findTarget(nums,p,n,target);   
+            
+         if(index==-1){
+					return -1;
+				}
+				
+         else{
+			return index;
+		}             
     }
 };

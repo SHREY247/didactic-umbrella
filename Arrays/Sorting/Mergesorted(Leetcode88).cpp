766bb29f335994095ad7f95baf9f9eaad3da7c67
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-07-21 03:24:41 +0530

    Update Mergesorted(Leetcode88).cpp

--- Arrays/Sorting/Mergesorted(Leetcode88).cpp
+++ Arrays/Sorting/Mergesorted(Leetcode88).cpp
@@ -9,15 +9,20 @@ and the last n elements are set to 0 and should be ignored. nums2 has a length o
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //Filling  nums1 vector from the end
         int i=m-1,j=n-1,k=m+n-1;
         while(i>=0&&j>=0)
-        {
+        {   
+            //If last element of nums1 is greater than last element of nums2, then the larger one would come at the end
+            //nums1[i] is filled at the last slot and we update the values of i and k and the loop goes on...
             if(nums1[i]>nums2[j])
             {
                 nums1[k]=nums1[i];
                 i--;
                 k--;
             }
+
+            //If last element of nums2 is greater
             else
             {
                 nums1[k]=nums2[j];
@@ -25,11 +30,13 @@ public:
                 k--;
             }
         }
+        //Case when some elements are left in nums1
         while(i>=0)
             nums1[k--]=nums1[i--];
+
+        //Case when some elements are left in nums2    
         while(j>=0)
             nums1[k--]=nums2[j--];
     }
 };
-
  

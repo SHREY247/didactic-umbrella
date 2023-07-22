106fc3d5e2dfd684029e77edfd0ac84ae214cc3d
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-07-22 21:49:51 +0530

    Create SortColors(Leetcode75).cpp

---
+++ Arrays/Sorting/SortColors(Leetcode75).cpp
@@ -0,0 +1,31 @@
+/*
+Given an array nums with n objects colored red, white, or blue,
+sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+*/
+
+//O(N) approach: Dutch National Flag problem
+class Solution {
+public:
+    //Keep all 0s before low pointer
+    //Keep all the 2s after high pointer
+    //all the 1s between low and high
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};

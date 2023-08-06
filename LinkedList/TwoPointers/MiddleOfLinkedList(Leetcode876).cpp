f88facdd3d28913dde6a5e7f10cbffd67f1555b1
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-08-06 17:27:49 +0530

    Update MiddleOfLinkedList(Leetcode876).cpp

--- LinkedList/TwoPointers/MiddleOfLinkedList(Leetcode876).cpp
+++ LinkedList/TwoPointers/MiddleOfLinkedList(Leetcode876).cpp
@@ -25,13 +25,34 @@ Approach
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        
+        //Use 2 pointers, slow and fast
+        //Initially, slow pointer as well as fast pointer point to head
+        //slow pointer hops by just 1 unit while fast pointer hops by 2 units (or nodes)
+        //So, when fast pointer would reach the end, slow would be at the mid
+
+
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast != NULL && fast->next != NULL){
+        //We are checking for two conditions
+        //One, when the number of nodes are odd: In this case termination condition would be: fast->next==NULL 
+        //Second, in case of even number of nodes, the termination condition would be fast ==NULL
+
+        //So run the while loop only when fast!= NULL && fast->next!=NULL
+        //In this way, we'll handle both the cases in a single loop
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }

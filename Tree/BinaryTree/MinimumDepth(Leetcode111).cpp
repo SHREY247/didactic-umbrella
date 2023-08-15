bab06398132f38a946aa13dc36f842f126fbd302
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-08-16 04:07:28 +0530

    Update MinimumDepth(Leetcode111).cpp

--- Tree/BinaryTree/MinimumDepth(Leetcode111).cpp
+++ Tree/BinaryTree/MinimumDepth(Leetcode111).cpp
@@ -9,29 +9,45 @@ Output: 2
 
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        // Base case...
-        // If the subtree is empty i.e. root is NULL, return depth as 0...
-        if(root == NULL)  return 0;
-        // Initialize the depth of two subtrees...
-        int leftDepth = minDepth(root->left);
-        int rightDepth = minDepth(root->right);
-        // If the both subtrees are empty...
-        if(root->left == NULL && root->right == NULL)
+        //If root is NULL, return 0
+        if(!root){
+            return 0;
+        }
+
+        //If leaf node, return height as 1
+        if(!root->left && !root->right) {
             return 1;
-        // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
-        if(root->left == NULL)
-            return 1 + rightDepth;
-        // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
-        if(root->right == NULL)
-            return 1 + leftDepth;
-        // When the two child function return its depth...
-        // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
-        return min(leftDepth, rightDepth) + 1;    // Adding 1 is the current node which is the parent of the two subtrees...
+        }
+
+        //Initialize ans as maximum possible value
+        int ans=INT_MAX;
+
+        //If root->left exists, calculate minDepth of left sub tree
+        if(root->left){
+            ans=min(minDepth(root->left),ans);
+        }
+        
+        //If root->right exists, calculate minDepth of right sub tree
+        if(root->right){
+            ans=min(minDepth(root->right),ans);
+        }
+        
+        //1 added to add contribution of root node
+        return ans+1;
     }
 };
-
-
  

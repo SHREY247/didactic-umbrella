e8884efff6be40a896282e3c1b5b856ed895ebdb
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-08-19 23:07:45 +0530

    Update BinaryTreePreOrderTraversal(Leetcode144).cpp

--- Tree/BinaryTree/BinaryTreePreOrderTraversal(Leetcode144).cpp
+++ Tree/BinaryTree/BinaryTreePreOrderTraversal(Leetcode144).cpp
@@ -13,6 +13,9 @@ Given the root of a binary tree, return the preorder traversal of its nodes' val
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//Recursive approach
 class Solution {
 public:
     vector<int>ans;
@@ -25,3 +28,39 @@ public:
         return ans;
     }
 };
+
+
+//Iterative approach using stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> v;
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(!stk.empty()){
+            TreeNode * temp = stk.top();
+            stk.pop();
+            v.push_back(temp->val);
+            //Stack is LIFO structure, so as we want right->val in the end, we would push it first
+            if(temp->right) stk.push(temp->right);
+            if(temp->left) stk.push(temp->left);
+        }
+
+        return v;
+    }
+};
+
+
+

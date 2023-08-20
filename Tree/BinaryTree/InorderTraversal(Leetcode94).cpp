4dd5a46622d8bb99d757bacb4df6a8c8f8bcf765
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-08-20 18:14:40 +0530

    Update InorderTraversal(Leetcode94).cpp

--- Tree/BinaryTree/InorderTraversal(Leetcode94).cpp
+++ Tree/BinaryTree/InorderTraversal(Leetcode94).cpp
@@ -16,6 +16,54 @@ Inorder Traversal Order: Left Sub Tree, Root, Right Sub Tree
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+//Iterative approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ //Iterative method using stack
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        stack<TreeNode*> stk;
+        //Keep on pushing root into stack  and update it to root->left until root becomes NULL
+
+        while(root){
+            stk.push(root);
+            root = root->left;
+        }
+
+        //Value of top of the stack is inserted into the vector because its left sub tree is null
+        //Then do inorder traversal for right sub tree of this node
+        while(!stk.empty()){
+            v.push_back(stk.top()->val);
+            TreeNode* temp = stk.top()->right;
+            stk.pop();
+            //Inorder traversal of right sub tree
+            while(temp){
+                stk.push(temp);
+                temp = temp->left;
+            }
+        }
+        return v;
+ 
+    }
+};
+
+
+
+//Recursive approach
 class Solution {
 public:
     vector<int> ans;

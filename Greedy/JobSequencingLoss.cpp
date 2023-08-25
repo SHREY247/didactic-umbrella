4aaaf75705ecac3c72226fa6f0f0d71bef9d9456
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-08-26 03:14:07 +0530

    Create JobSequencingLoss.cpp

---
+++ Greedy/JobSequencingLoss.cpp
@@ -0,0 +1,61 @@
+/*
+Minimize the losses: Job Sequencing
+*/
+
+//We need to do the activity with highest loss per day as early as possible so as to minimize the overall loss
+//But we also need to a short activity as early as possible, considering all jobs have same penalty
+//Greed : (Loss/Days to complete)
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define all(c) c.begin(), c.end()
+
+
+typedef pair<int, pair<int, int> > job;
+
+// compare function is given so that we can specify
+// how to compare a pair of jobs
+bool cmp_pair(job a, job b)
+{
+	int a_Li, a_Ti, b_Li, b_Ti;
+	a_Li = a.second.first;
+	a_Ti = a.second.second;
+	b_Li = b.second.first;
+	b_Ti = b.second.second;
+
+	// To compare a/b and c/d, compare ad and bc
+	return (a_Li * b_Ti) > (b_Li * a_Ti);
+}
+
+void printOptimal(int L[], int T[], int N)
+{
+	vector<job> list; // (Job Index, Si, Ti)
+
+	for (int i = 0; i < N; i++) {
+		int t = T[i];
+		int l = L[i];
+
+		// Each element is: (Job Index, (Li, Ti) )
+		list.push_back(make_pair(i + 1, make_pair(l, t)));
+	}
+
+	stable_sort(all(list), cmp_pair);
+
+	// traverse the list and print job numbers
+	cout << "Job numbers in optimal sequence are\n";
+	for (int i = 0; i < N; i++)
+		cout << list[i].first << " ";
+
+}
+
+// Driver code
+int main()
+{
+	int L[] = { 1, 2, 3, 5, 6 };
+	int T[] = { 2, 4, 1, 3, 2 };
+	int N = sizeof(L) / sizeof(L[0]);
+	printOptimal(L, T, N);
+	return 0;
+}

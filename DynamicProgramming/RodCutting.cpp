3596f1242fcb1ddcd92a0134ae0946340da90ee2
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-09-01 01:18:53 +0530

    Update RodCutting.cpp

--- DynamicProgramming/RodCutting.cpp
+++ DynamicProgramming/RodCutting.cpp
@@ -54,3 +54,56 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+
+
+/* Bottom Top
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int cutRod(int price[], int n) {
+        vector<int> dp(n + 1, 0);
+        
+        for (int i = 1; i <= n; ++i) {
+            int max_value = -1;
+            for (int j = 1; j <= i; ++j) {
+                max_value = max(max_value, price[j - 1] + dp[i - j]);
+            }
+            dp[i] = max_value;
+        }
+        
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+*/

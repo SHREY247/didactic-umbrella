04bf96dab145123384f731b4050145ac646fb4b9
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-09-13 17:41:58 +0530

    Update TwoSum(Leetcode1).cpp

--- Arrays/TwoPointer/TwoSum(Leetcode1).cpp
+++ Arrays/TwoPointer/TwoSum(Leetcode1).cpp
@@ -4,26 +4,39 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 
+/*We store the numbers and their actual indices in a vector of pairs. 
+Then we sort the vector on the basis of numbers. Now, we maintain 2 pointers, first at the starting and second one at the last of the vector. 
+Now, check if sum equals target, return the actual indices, else if sum less than target, increase the left pointer, else if target is lesser than sum, 
+decrement the right pointer.
+
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
+*/
+    
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        //Push the value and index in vector vp
-        vector<pair<int,int>>vp;
-        for(int i=0; i<n; i++){
-            vp.push_back({nums[i],i});
+        int n=nums.size();
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++){
+            v.push_back({nums[i],i});
         }
-        //Sort the vector
-        sort(vp.begin(), vp.end());
-        int i = 0, j = n-1;
-        while(i < j){
-            int sum = vp[i].first + vp[j].first;
-            //if sum equals target return the indices
-            if(sum == target) return {vp[i].second,vp[j].second};
-            //If sum is less than target, increment i necause that would increase our sum
-            if(sum < target) i++;
-            //If sum is less than target, decrement j, that way sum would decrease and we can check whether it becomes equal to target
-            else j--;
+
+        sort(v.begin(),v.end());
+        int l=0,r=n-1;
+        while(l<r){
+            int sum=v[l].first + v[r].first;
+            if(sum==target){
+                return {v[l].second, v[r].second};
+            }
+
+            else if(sum<target){
+                l++;
+            }   
+
+            else{
+                r--;
+            }        
         }
         return {};
     }

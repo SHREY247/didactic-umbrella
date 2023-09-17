02af199474ea3e1de576262286edd1856ac82996
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-09-18 03:20:31 +0530

    Update BesttimetoBuyandSellStock(Leetcode121).cpp

--- DynamicProgramming/BesttimetoBuyandSellStock(Leetcode121).cpp
+++ DynamicProgramming/BesttimetoBuyandSellStock(Leetcode121).cpp
@@ -15,13 +15,19 @@ Note that buying on day 2 and selling on day 1 is not allowed because you must b
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int buyAtPrice = INT_MAX;
-        int maxProfit = 0;
-        for(int i=0; i<n; i++){
-            buyAtPrice = min(buyAtPrice, prices[i]);
-            maxProfit = max(maxProfit, prices[i] - buyAtPrice); 
-        }
-        return maxProfit;
+    int n = prices.size();
+    if (n <= 1) return 0;
+    
+    int min_price = prices[0];
+    vector<int> dp(n, 0);
+    
+    for (int i = 1; i < n; i++) {
+        min_price = min(min_price, prices[i]);
+        dp[i] = max(dp[i-1], prices[i] - min_price);
     }
+    
+    return dp[n-1];
+}
+
 };
+

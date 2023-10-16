adfe59506b0abcb42f1ad2d6a91f8ed9a9b2e986
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-10-17 04:07:16 +0530

    Update 32_CloneGraph.cpp

--- Grind75/32_CloneGraph.cpp
+++ Grind75/32_CloneGraph.cpp
@@ -10,8 +10,37 @@ Test case format:
 For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
 An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
- */
 
+
+Intuition
+Use DFS approach to traverse the original graph. During the traversal,create a new node for each encountered node and maintain a mapping between the original nodes and their corresponding clones. When a node's neighbors are processed, check if the clone of the neighbor node has already been created. If so, use the clone from the mapping. Otherwise, recursively explore the neighbor.
+
+Approach
+1: Define a helper function dfs that takes a node cur from the original graph and a mapping mp which will store the relationship between original nodes and their clones.
+2: In the dfs function:
+a) Create a new node clone with the same value as cur.
+b) Add the mapping between cur and clone.
+c) Initialize an empty vector neighbour to store the cloned neighbors.
+d)Iterate through the neighbors of cur.
+1) If the neighbor has already been cloned (mp.find(it) != mp.end()), use the clone from the mapping.
+2) Otherwise, recursively call dfs on the neighbor to get its clone.
+3) Set the neighbors of the clone to the neighbour vector.
+4) Return the clone.
+
+3: In the cloneGraph function:
+1)Initialize an unordered map mp to store the mapping between original nodes and their clones.
+2)Check if node is NULL, and if so, return NULL.
+3) Call the dfs function with the original node and the mapping mp.
+
+Complexity
+Time complexity:
+O(V+E)
+
+Space complexity:
+O(V)
+
+Code
+*/
 class Solution {
 public:
 Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
@@ -38,3 +67,4 @@ Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
             
         return dfs(node,mp);
     }};
+ 

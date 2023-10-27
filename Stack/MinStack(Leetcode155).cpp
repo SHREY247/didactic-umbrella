56f9fc67ed7724d7a94a9c73fdd157a7cd0486e8
Shrey Kakkar <108192833+SHREY247@users.noreply.github.com>
2023-10-28 01:40:58 +0530

    Update MinStack(Leetcode155).cpp

--- Stack/MinStack(Leetcode155).cpp
+++ Stack/MinStack(Leetcode155).cpp
@@ -9,44 +9,60 @@ void pop() removes the element on the top of the stack.
 int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
-
 */
 
+Intuition
+The MinStack class is designed to maintain a stack of elements while also keeping track of the minimum element in the stack.
+
+Approach
+1: The MinStack class uses a single stack, stk, to store pairs of elements and the minimum values encountered so far.
+2: When an element is pushed onto the stack, the current minimum value is calculated by comparing the new element with the minimum value stored on top of the stack. The pair (element, current minimum) is then pushed onto the stack.
+3: When an element is popped from the stack, it effectively removes the top pair, effectively reverting to the previous state.
+4: The top function returns the first element of the top pair on the stack, and the getMin function returns the second element of the top pair, which represents the current minimum value.
+
+Complexity
+Time complexity:
+O(1) for all operations
+
+Space complexity:
+O(N)
+
+Code
+*/
 class MinStack {
 public:
-//Stack of type pair
-//First one points to the value
-//Second one points to the current minima
-stack<pair<int , int>> stk;
+    //Pair contains the current value and the current minima 
+    stack<pair<int,int>> stk;
     MinStack() {
-        stack<pair<int , int>> stk;
+        
     }
- 
+    
     void push(int val) {
-        //If empty stack, push value and the minima would be same as value for only 1 element in the stack
         if(stk.empty()){
-            stk.push({val , val});
+            stk.push({val,val});
         }
-        //Push current element and the current minima
+
         else{
-            int mn = min( val , stk.top().second);
-            stk.push({val , mn});
-        }
+            int mini=min(val,stk.top().second);
+            stk.push({val,mini});
+        }        
     }
- 
+    
     void pop() {
-        if(!stk.empty()) stk.pop();
+        if(!stk.empty()){
+            stk.pop();
+        }        
     }
- 
+    
     int top() {
-        return(stk.top().first);
+        return(stk.top().first);        
     }
- 
+    
     int getMin() {
-        return stk.top().second;
+        return(stk.top().second);            
     }
 };
- 
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
